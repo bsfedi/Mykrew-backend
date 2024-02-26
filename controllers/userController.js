5a216fca60d06c1a9bc8b06c66769cf180d7b516
bsfedi <65951975+bsfedi@users.noreply.github.com>
2024-02-26 20:31:21 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -780,10 +780,7 @@ exports.addPDFtoUser = async (req, res) => {
       { "missions._id": missionId },
       {
         $push: {
-          "missions.$.craInformation.craPDF": {
-            filename: craPdfFilename,
-            date: currentDate // Add the current date to the PDF object
-          }
+          "missions.$.craInformation.craPDF": JSON.stringify({ filename: craPdfFilename, date: currentDate })
         },
       },
       { new: true }
@@ -803,6 +800,7 @@ exports.addPDFtoUser = async (req, res) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
+
 
 exports.getAllCras = async (req, res) => {
 
